91b27643fb755b4b966fc589e562fb548570b012
AlbertoPavarin <72099114+AlbertoPavarin@users.noreply.github.com>
2021-05-28 23:14:12 +0200

    Caricamento progetto

---
+++ Space_Invaders/Space_Invaders/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Space_Invaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.nuovaPartitaBtn = new System.Windows.Forms.Button();
+            this.RegoleBtn = new System.Windows.Forms.Button();
+            this.PunteggioTxt = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerPb = new System.Windows.Forms.PictureBox();
+            this.utenteTxt = new System.Windows.Forms.TextBox();
+            this.salvaBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ClassificaDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClassificaDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nuovaPartitaBtn
+            // 
+            this.nuovaPartitaBtn.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.nuovaPartitaBtn, "nuovaPartitaBtn");
+            this.nuovaPartitaBtn.Name = "nuovaPartitaBtn";
+            this.nuovaPartitaBtn.UseVisualStyleBackColor = false;
+            this.nuovaPartitaBtn.Click += new System.EventHandler(this.button1_Click);
+            this.nuovaPartitaBtn.MouseHover += new System.EventHandler(this.nuovaPartitaBtn_MouseHover);
+            // 
+            // RegoleBtn
+            // 
+            this.RegoleBtn.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.RegoleBtn, "RegoleBtn");
+            this.RegoleBtn.Name = "RegoleBtn";
+            this.RegoleBtn.UseVisualStyleBackColor = false;
+            this.RegoleBtn.Click += new System.EventHandler(this.RegoleBtn_Click);
+            this.RegoleBtn.MouseHover += new System.EventHandler(this.RegoleBtn_MouseHover);
+            // 
+            // PunteggioTxt
+            // 
+            resources.ApplyResources(this.PunteggioTxt, "PunteggioTxt");
+            this.PunteggioTxt.ForeColor = System.Drawing.Color.Yellow;
+            this.PunteggioTxt.Name = "PunteggioTxt";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimer);
+            // 
+            // playerPb
+            // 
+            this.playerPb.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.playerPb, "playerPb");
+            this.playerPb.Name = "playerPb";
+            this.playerPb.TabStop = false;
+            // 
+            // utenteTxt
+            // 
+            resources.ApplyResources(this.utenteTxt, "utenteTxt");
+            this.utenteTxt.Name = "utenteTxt";
+            this.utenteTxt.MouseClick += new System.Windows.Forms.MouseEventHandler(this.utenteTxt_MouseClick);
+            // 
+            // salvaBtn
+            // 
+            this.salvaBtn.BackColor = System.Drawing.Color.Black;
+            this.salvaBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            resources.ApplyResources(this.salvaBtn, "salvaBtn");
+            this.salvaBtn.Name = "salvaBtn";
+            this.salvaBtn.UseVisualStyleBackColor = false;
+            this.salvaBtn.Click += new System.EventHandler(this.salvaBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ClassificaDGV
+            // 
+            this.ClassificaDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ClassificaDGV.BackgroundColor = System.Drawing.Color.Black;
+            this.ClassificaDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ClassificaDGV.DefaultCellStyle = dataGridViewCellStyle1;
+            resources.ApplyResources(this.ClassificaDGV, "ClassificaDGV");
+            this.ClassificaDGV.Name = "ClassificaDGV";
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            this.ClassificaDGV.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.salvaBtn);
+            this.Controls.Add(this.utenteTxt);
+            this.Controls.Add(this.PunteggioTxt);
+            this.Controls.Add(this.playerPb);
+            this.Controls.Add(this.RegoleBtn);
+            this.Controls.Add(this.nuovaPartitaBtn);
+            this.Controls.Add(this.ClassificaDGV);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.playerPb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClassificaDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nuovaPartitaBtn;
+        private System.Windows.Forms.Button RegoleBtn;
+        private System.Windows.Forms.PictureBox playerPb;
+        private System.Windows.Forms.Label PunteggioTxt;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.TextBox utenteTxt;
+        private System.Windows.Forms.Button salvaBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView ClassificaDGV;
+    }
+}
+
---
+++ Space_Invaders/Space_Invaders/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+/*Autore: Pavarin Alberto
+ * Questo programma permette di giocare a space invaders
+ */
+namespace Space_Invaders
+{
+    public partial class Form1 : Form
+    {
+        bool destra, sinistra;
+        int velocitaPlayer = 20;
+        int velocitàNemici = 10;
+        int punteggio = 0;
+        int timerProiettiliNemici = 300;
+
+        PictureBox[] Invaders;
+
+        DataTable table = new DataTable(); 
+
+        bool sparo;
+        bool isgameOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Properties.Resources.Space_Invaders_intro;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.Space_Invaders_Gioco;
+            nuovaPartitaBtn.Visible = false;
+            RegoleBtn.Visible = false;
+            playerPb.BackgroundImage = Properties.Resources.player;
+            PunteggioTxt.Visible = true;
+            RegoleBtn.Enabled = false;
+            setup();
+        }
+
+        private void RegoleBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Le regole del gioco sono:\n1) Lo scopo del gioco è di distruggere gli invaders senza che loro ti distruggano o ti sorpassino.\n2) Per muoversi a destra bisogna premere il tasto 'freccia destra' e per muoversi a sinistra il tasto 'freccia sinistra'.\n3) Per sparare ai nemici bisogna premere la freccia in su.");
+        }
+
+        private void nuovaPartitaBtn_MouseHover(object sender, EventArgs e)
+        {
+            nuovaPartitaBtn.Cursor = Cursors.Hand; //Quando il cursore del mouse è sopra il bottone il cursore cambierà forma prendendo quella di una mano
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left) // Se il tasto premuto è uguale al tasto A allora la variabile booleana sinistra verrà inizializzata a true
+            {
+                sinistra = true;               
+            }
+            if (e.KeyCode == Keys.Right) // Se il tasto premuto è uguale al tasto D allora la variabile booleana destra verrà inizializzata a true
+            {
+                destra = true;
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) // Se la freccia sinistra non è premuto allora la variabile booleana sinistra sarà inizializzata a false
+            {
+                sinistra = false;
+            }
+            if (e.KeyCode == Keys.Right) // Se la freccia destra non è premuto allora la variabile booleana destra sarà inizializzata a false
+            {
+                destra = false;
+            }
+            if (e.KeyCode == Keys.Up && sparo == false) // Se il tasto premuto è la freccia in su la variabile booleana sparo verrà inizializzata a true e verrà creato il proiettile tramite la funzione apposita
+            {
+                sparo = true;
+                creazioneProiettili("proiettile");
+            }
+            if (e.KeyCode == Keys.Enter && isgameOver == true)
+            {
+                elimina();
+                setup();
+            }
+        }
+
+        private void creazioneInvaders()
+        {
+            Invaders = new PictureBox[25]; // Viene creata un array di PicturBox in modo da "ospitare" gli inavders
+            int left = 0;
+
+            for (int i = 0; i < Invaders.Length; i++)
+            {
+                Invaders[i] = new PictureBox();
+                Invaders[i].Size = new Size(50, 50); // Viene inizializzata la grandezza di ogni Picturbox a 60 e 50
+                Invaders[i].Image = Properties.Resources.invader1; // Viene assegnata l'immagine dell'invaders a ogni PictureBox
+                Invaders[i].Top = 5; // Viene impostata a 5 la distanza dal bordo superiore   
+                Invaders[i].Tag = "Invaders";
+                Invaders[i].Left = left; // Viene impostata a left 
+                Invaders[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(Invaders[i]);
+                left = left - 80; 
+            }
+        }
+
+        private void RegoleBtn_MouseHover(object sender, EventArgs e)
+        {
+            RegoleBtn.Cursor = Cursors.Hand; // Quando il cursore è sopra il bottone il cursore assumerà la forma di un mano
+        }
+
+        private void mainGameTimer(object sender, EventArgs e)
+        {
+            PunteggioTxt.Text = "Punteggio: " + punteggio; // Viene scritto nella TextBox PunteggioTxt Il punteggio 
+
+            if (sinistra) // Se sistra è uguale a true il player verrà spostato a sinistra di quanto è il valore di velocità player 
+            {
+                playerPb.Left -= velocitaPlayer;
+            }
+
+            if (destra) // Se sistra è uguale a true il player verrà spostato a sinistra di quanto è il valore di velocità player
+            {
+                playerPb.Left += velocitaPlayer;
+            }
+
+            if (playerPb.Left > 750 || playerPb.Left < 10) // Se il player va oltre il form viene riportato nella posizione iniziale
+            {
+                playerPb.Left = 370;
+            }
+
+            timerProiettiliNemici -= 10;
+
+            if (timerProiettiliNemici < 1) // Se il timer dei proiettili è minore di 1 allora il timer viene risettato a 300 e viene creato il proiettile
+            {
+                timerProiettiliNemici = 300;
+                creazioneProiettili("proiettileInvader");
+            }
+
+            foreach (Control x in this.Controls) // Per ogni x 
+            {
+                if (x is PictureBox && (string)x.Tag == "Invaders") // Se la x è una PicturBox e il tag è Invaders e quindi se la x è un nemico
+                {
+                    x.Left += velocitàNemici; // Viene spostato il nemico
+                    if (x.Left > 730) // Se la distanza dal bordo sinistro è maggiore di 730 gli invaders saranno portati all'inizio e spostati verso il basso
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Top > playerPb.Top) // Se gli invaders si trovano al di sotto del giocatore allora sarà mostrato il messaggio di gameover
+                    {
+                        gameOver("Gli invaders ti hanno superato, il mondo verrà conquistato");
+                    }
+
+                    if (x.Bounds.IntersectsWith(playerPb.Bounds)) // Se gli invaders collidono con il player viene mostrato il messaggio di game over
+                    {
+                        gameOver("Sei stato ucciso dagli Invaders. Ora il mondo verrà conquistato dagli invasori.");
+                    }
+
+                    foreach (Control y in this.Controls) // per ogni y
+                    {
+                        if (y is PictureBox && (string)y.Tag == "proiettile") // Se y è una PictureBox e il tag è proiettili e quindi y è un proiettile
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds)) // Se il proiettile collide con l'invaders allora sia il proiettile che il nemico vengono rimossi e il punteggio viene incrementato di uno 
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                punteggio ++;
+                                sparo = false;
+                            }
+                        }
+                    }
+                }
+                if (x is PictureBox && (string)x.Tag == "proiettile") // Se è un proiettile 
+                {
+                    x.Top -= 40; // Viene spostato il proiettile 
+
+                    if (x.Top < 15) // Se la distanza è maggiore di 15 il proiettile viene eliminato
+                    {
+                        this.Controls.Remove(x);
+                        sparo = false;
+                    }
+                    if (punteggio > 15)
+                    {
+                        x.Top -= 25;
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "proiettileInvader") // Se x è un proiettile nemico 
+                {
+                    x.Top += 20; // viene spostato il proiettile
+
+                    if (x.Top > 620) // Se la distanza è maggiore di 620 allora il proiettile viene rimosso
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(playerPb.Bounds)) // Se il proiettile colpisce il giocatore viene rimosso il proiettile e viene mostrato il messaggio di game over
+                    {
+                        this.Controls.Remove(x);
+                        gameOver("Sei stato ucciso dagli Invaders. Ora il mondo verrà conquistato dagli invasori.");
+                    }
+                }
+            }
+            if (punteggio > 7) // Se il punteggio è maggiore di 7 allora la velocità dei nemici viene impostata a 15
+            {
+                velocitàNemici = 15;
+                if (punteggio > 20) // Se il punteggio è maggiore di 20 allore la velocità dei nemici viene impostata a 22
+                {
+                    velocitàNemici = 19;
+                }
+            }
+            if (punteggio == Invaders.Length) // Se tutti gli inveders sono stati distrutti allora viene mostrato il messaggio di gameover
+            {
+                gameOver("Hai salvato il mondo!");
+            }
+        }
+
+        private void setup()
+        {
+            PunteggioTxt.Text = "Punteggio: 0";
+            punteggio = 0;
+            isgameOver = false;
+
+            timerProiettiliNemici = 200;
+            velocitàNemici = 5;
+            sparo = false;
+
+            creazioneInvaders();
+            gameTimer.Start();
+        }
+
+        private void gameOver(string message)
+        {
+            isgameOver = true;
+            gameTimer.Stop();
+            PunteggioTxt.Text = "Punteggio:  " + punteggio + "         " + message;
+            utenteTxt.Visible = true;
+        }
+
+        private void elimina()
+        {
+            foreach (PictureBox i in Invaders)
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "proiettile" || (string)x.Tag == "proiettileInvader")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void utenteTxt_MouseClick(object sender, MouseEventArgs e)
+        {
+            utenteTxt.Clear();
+            salvaBtn.Visible = true; 
+        }
+        
+        private void salvaBtn_Click(object sender, EventArgs e)
+        {
+            string file = @"Punteggi.txt"; // Viene creato il nome file
+            string percorso = AppDomain.CurrentDomain.BaseDirectory + file; // Viene creato il percorso per salvare il file
+            do // Fichè il giocatore non inserisce un nome utente vien richiesto 
+            {
+                if (utenteTxt.Text != "")
+                {
+                    table.Clear();
+                    salvaBtn.Visible = false;
+                    utenteTxt.Visible = false;
+                    StreamWriter punteggi = new StreamWriter(percorso, true); // Vengono scritti i punteggi e i nomi utenti
+                    punteggi.WriteLine(utenteTxt.Text + "  -  " + punteggio);
+                    punteggi.Close();
+                    MessageBox.Show("Punteggio salvato con successo");
+                    ClassificaDGV.Visible = true;
+                    ClassificaDGV.Enabled = true;
+
+                    // Vengono aggiunti Nome Utente e Punteggio nella classifica dentro il Data grid View
+                    string [] linee = File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + file);
+                    string [] values;
+                    for (int i = 0; i < linee.Length; i++)
+                    {
+                        values = linee[i].ToString().Split('-'); // Viene messo nell'array values il contenuto di linee
+                        string[] righe = new string[values.Length];
+
+                        for (int j = 0; j < values.Length; j++)
+                        {
+                            righe[j] = values[j].Trim();/* Viene messo nell'array righe il contenuto di values e vengono poi messo nel DGV e i contenuti separati da "-" vengon messi in due colonne separate, 
+                                                        quindi il contenuto che c'è prima di "-" verrà messo nella colonna del Nome, mentre il contenuto che si trova dopo di "-" verrà scritto nella colonna del punteggio*/
+                        }
+
+                        table.Rows.Add(righe);
+                    }
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Inserisci un nome utente");
+                }
+            }
+            while (utenteTxt.Text != "");
+            button1.Visible = true;
+            button1.Enabled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // il pulsante "nouva partita?" permette di fare una nuova partita
+            elimina();
+            utenteTxt.Visible = false;
+            salvaBtn.Visible = false;
+            button1.Visible = false;
+            button1.Enabled = false;
+            playerPb.Visible = true;
+            ClassificaDGV.Visible = false;
+            ClassificaDGV.Enabled = false;
+            PunteggioTxt.Visible = true;
+            utenteTxt.Text = "Inserisci il tuo nome utente";
+            playerPb.Left = 370;
+            setup();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Vengono create le due colonne per il Nome Utente e il Punteggio
+            table.Columns.Add("Nome Utente", typeof(string));
+            table.Columns.Add("Punteggio", typeof(int));
+            ClassificaDGV.DataSource = table;
+        }
+
+        private void creazioneProiettili(string bulletTag)
+        {
+            PictureBox proiettili = new PictureBox(); // Viene creata una PicturBox 
+            proiettili.Image = Properties.Resources.bullet; // L'immagine della picturBox sarà il proiettile 
+            proiettili.Size = new Size(5, 20); // Viene impostata la grandezza della PicturBox
+            proiettili.Tag = bulletTag;
+            proiettili.Left = playerPb.Left + playerPb.Width / 2; // Viene impostata da dove partirà il proiettile
+
+            if ((string)proiettili.Tag == "proiettile")
+            {
+                proiettili.Top = playerPb.Top - 20;
+            }
+            else if ((string)proiettili.Tag == "proiettileInvader")
+            {
+                proiettili.Top = -100;
+            }
+
+            this.Controls.Add(proiettili);
+        }
+    }
+}
